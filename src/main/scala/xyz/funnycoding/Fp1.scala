f1cbae225ab22babd3914b3418d27024cb4b40b9
Nader Hajlaoui <hajlaoui.nader@gmail.com>
2022-04-12 09:48:41 +0200

    intro

--- src/main/scala/xyz/funnycoding/Fp1.scala
+++ src/main/scala/xyz/funnycoding/Fp1.scala
@@ -6,4 +6,13 @@ object Fp1 {
   case object PNil extends PList[Nothing]
   case class PCons[+A](head: A, tail: PList[A]) extends PList[A]
 
+  trait PFunctor[F[_]] {
+    def map[A, B](fa: F[A])(f: A => B): F[B]
+  }
+
+  trait PApplicative[F[_]] extends PFunctor[F] {
+    def pure[A](a: A): F[A]
+    def ap[A, B](fa: F[A])(f: F[A => B]): F[B]
+  }
+
 }
