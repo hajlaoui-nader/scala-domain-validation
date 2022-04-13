33d1aec3fe1f72776b4bf838c1e2a6800cd384f4
Nader Hajlaoui <hajlaoui.nader@gmail.com>
2022-04-13 15:19:01 +0200

    add option functor

--- src/main/scala/xyz/funnycoding/Fp1.scala
+++ src/main/scala/xyz/funnycoding/Fp1.scala
@@ -2,13 +2,25 @@ package xyz.funnycoding
 
 object Fp1 {
 
+  sealed trait POption[+A]
+  case object PNone extends POption[Nothing]
+  case class PSome[A](a: A) extends POption[A]
+
+  implicit val pOptionFunctor = new PFunctor[POption] {
+    def map[A, B](fa: POption[A])(f: A => B): POption[B] =
+      fa match {
+        case PNone    => PNone
+        case PSome(a) => PSome(f(a))
+      }
+  }
+
   sealed trait PList[+A]
   case object PNil extends PList[Nothing]
   case class PCons[+A](head: A, tail: PList[A]) extends PList[A]
 
-  implicit val plistFunctor = new Functor[PList] {
+  implicit val plistFunctor = new PFunctor[PList] {
     def map[A, B](fa: PList[A])(f: A => B): PList[B] = fa match {
-      case PNil => PNil
+      case PNil        => PNil
       case PCons(h, t) => PCons(f(h), map(t)(f))
     }
   }
@@ -38,9 +50,5 @@ object Fp1 {
     def traverse[A, B](fa: F[A])(f: A => F[B]): F[F[B]]
     def sequence[A](fas: F[F[A]]): F[F[A]] = traverse(fas)(identity)
   }
-
-
-
-
 
 }
